a24cddb866f61e9dc596c00b5ca153eb9a4b5cf4
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-15 01:39:18 -0500

    feat: add OrdersModule to app.module.ts

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,30 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+
+@Controller()
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @MessagePattern({ cmd: 'create_order' })
+  create(@Payload() createOrderDto: CreateOrderDto) {
+    return this.ordersService.create(createOrderDto);
+  }
+
+  @MessagePattern({ cmd: 'find_all_orders' })
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+  @MessagePattern({ cmd: 'find_one_order' })
+  findOne(@Payload() id: number) {
+    return this.ordersService.findOne(id);
+  }
+
+  @MessagePattern({ cmd: 'change_order_status' })
+  changeOrderStatus(@Payload() { id, status }: { id: number; status: string }) {
+    return this.ordersService.changeOrderStatus(id, status);
+  }
+}
