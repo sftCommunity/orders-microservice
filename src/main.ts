7cc16b99155aa228098b826382de5872adc1afd9
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-14 23:46:35 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.port);
+  logger.log(`Orders microservice running on port ${envs.port}`);
+}
+
+bootstrap();
