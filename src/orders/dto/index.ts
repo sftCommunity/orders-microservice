ac0de9402e75f1a6d0db89b49baae1374d24f5b9
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-15 19:09:57 -0500

    refactor: update CreateOrderDto to use OrderItemDto for handling order items

--- src/orders/dto/index.ts
+++ src/orders/dto/index.ts
@@ -1,5 +1,6 @@
 export * from './change-order-status.dto';
 export * from './create-order.dto';
+export * from './order-item';
 export * from './order-pagination';
 export * from './update-order.dto';
 
