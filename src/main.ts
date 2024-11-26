8e6489ce32c54aeda7ddb2466eeeed6d0f5155d4
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-26 03:40:33 -0500

    refactor: update logger name in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 import { envs } from './config/envs';
 
 async function bootstrap() {
-  const logger = new Logger('Main');
+  const logger = new Logger('OrdersMicroserviceMain');
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.NATS,
     options: {
